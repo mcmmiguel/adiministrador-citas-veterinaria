1c04179b60e333fe4f42e77e0f3f34dabd0fcdde
Miguel Cobian <miguelcobian99@gmail.com>
2023-05-06 21:21:04 -0600

    Funcionalidad de Editar Cita finalizada

--- js/app.js
+++ js/app.js
@@ -25,6 +25,10 @@ class Citas {
     eliminarCita(id) {
         this.citas = this.citas.filter(cita => cita.id !== id);
     }
+
+    editarCita(citaActualizada) {
+        this.citas = this.citas.map(cita => cita.id === citaActualizada.id ? citaActualizada : cita);
+    }
 }
 
 class UI {
@@ -177,7 +181,9 @@ function nuevaCita(e) {
         ui.imprimirAlerta('Editado correctamente', '');
 
         // Pasar el objeto de cita a edicion
+        administrarCitas.editarCita({...citaObj});
 
+        // Regresa el texto del boton a su estado original
         formulario.querySelector('button[type="submit"]').textContent = 'Crear cita';
 
         // Quitar el modo edicion
